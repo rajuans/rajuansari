b80b4d11bd1dd63c9852ee4d5a0286225a454768
Raju Ansari <raju.bhucs@gmail.com>
2026-06-25 23:21:56 -0700

    update site base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://rajuans.github.io',
-  base: '/rajuansari',
+  site: 'https://rajuansari.com',
   integrations: [tailwind(), mdx(), sitemap()],
 });
